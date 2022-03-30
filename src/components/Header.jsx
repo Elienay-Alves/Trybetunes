e745c3ab18669edfbd1563020cc46b877b32f74a
Elienay-Alves <elienay8486@gmail.com>
2022-03-30 20:02:37 -0300

    Requisito 2 Feito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,74 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from '../pages/Loading';
+import searchLogo from '../images/searchLogo.png';
+import styles from './header.module.css';
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      name: '',
+    };
+  }
+
+  componentDidMount() {
+    this.getname();
+  }
+
+  async getname() {
+    const response = await getUser();
+    if (response.name !== undefined) {
+      this.setState({
+        loading: false,
+        name: response.name,
+      });
+    }
+  }
+
   render() {
+    const { name, loading } = this.state;
     return (
-      <div />
+      <header
+        data-testid="header-component"
+      >
+        {loading && (<Loading />)}
+        {!loading && (
+          <div className={ styles.header_container }>
+            <div className={ styles.header_top }>
+              <img src={ searchLogo } alt="logo TrybeTunes" className="image-Header" />
+              <span
+                className={ styles.name_container }
+                data-testid="header-user-name"
+              >
+                { name }
+              </span>
+            </div>
+            <div className={ styles.header_bot }>
+              <Link
+                className={ styles.link }
+                to="/search"
+              >
+                Pesquisa
+              </Link>
+              <Link
+                className={ styles.link }
+                to="/favorites"
+              >
+                Favoritos
+              </Link>
+              <Link
+                className={ styles.link }
+                to="/profile"
+              >
+                Perfil
+              </Link>
+            </div>
+          </div>
+        )}
+      </header>
     );
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import Loading from './Loading';
+import styles from './login.module.css';
+import loginImage from '../images/loginImage.png';
 
 class Login extends Component {
   constructor() {
@@ -64,23 +66,25 @@ class Login extends Component {
     }
     return (
       <div
-        // className={ styles.body }
+        className={ styles.body }
         data-testid="page-login"
       >
+        <img src={ loginImage } alt="trybeTunes logo" />
         <form
           action=""
-          // className={ styles.form }
+          className={ styles.form }
         >
           <input
-            // className={ styles.input }
+            className={ styles.input }
             data-testid="login-name-input"
             type="text"
             onChange={ this.handleChange }
             name="name"
             value={ name }
+            placeholder="Nome"
           />
           <button
-            // className={ styles.button }
+            className={ styles.button }
             data-testid="login-submit-button"
             type="button"
             onClick={ this.handleClick }
