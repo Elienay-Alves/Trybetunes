885cc591efe8087f07285992c0e6c42537405795
Elienay-Alves <elienay8486@gmail.com>
2022-03-30 22:01:08 -0300

    Requisito 5 Feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,61 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      disable: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.validation();
+    });
+  }
+
+  validation = () => {
+    const { search } = this.state;
+    const charactersNumber = 1;
+    if (search.length > charactersNumber) {
+      this.setState({
+        disable: false,
+      });
+    } else {
+      this.setState({
+        disable: true,
+      });
+    }
+  }
+
   render() {
+    const { search, disable } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <main>
+          <form>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleChange }
+              name="search"
+              value={ search }
+            />
+            <button
+              data-testid="search-artist-button"
+              onClick={ this.handleClick }
+              type="button"
+              disabled={ disable }
+            >
+              Search
+            </button>
+          </form>
+        </main>
       </div>
     );
   }
